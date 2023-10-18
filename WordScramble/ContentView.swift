0d50059bedc610aeddcfee5e4fa22edc1811e9bd
IvanTilev <iivantilev@gmail.com>
2023-10-18 22:09:51 +0300

    Creating the logic of validating the words that the user type and checks them agains the random word.

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -12,6 +12,10 @@ struct ContentView: View {
     @State private var rootWord = ""
     @State private var newWord = ""
     
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    
     var body: some View {
         NavigationView {
             List {
@@ -32,6 +36,11 @@ struct ContentView: View {
                 .navigationTitle(rootWord)
                 .onSubmit(addNewWord)
                 .onAppear(perform: startGame)
+                .alert(errorTitle, isPresented: $showingError) {
+                    Button("OK", role: .cancel) {}
+                } message: {
+                    Text(errorMessage)
+                }
         }
     }
     
@@ -40,7 +49,22 @@ struct ContentView: View {
         let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
         // 2. Check if there is more than 1 letter in the answer
         guard answer.count > 0 else { return }
+        
         // 3. Exttra validation to come
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already used", message: "Be more original")
+            return
+        }
+        
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not possible", message: "You can't spell this word from \(rootWord)")
+            return
+        }
+        
+        guard isReal(word: answer) else {
+            wordError(title: "Word not recognised", message: "You can't just made them up, you know!")
+            return
+        }
         
         // 4. Insert the answet to the array of answer to keep track of them
         withAnimation {
@@ -61,6 +85,40 @@ struct ContentView: View {
         }
         
         fatalError("Could not load start.txt from bundle.")
+    }
+    
+    // Function that will check if the typed word is already used
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    // Function that will checks if the word if possible from the random word
+    func isPossible(word: String) -> Bool {
+         var tempWord = rootWord
+         
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    // Function that checks users string for misspelled words
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
     }
 }
 
